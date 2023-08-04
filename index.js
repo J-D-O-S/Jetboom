2c4014864f1a1bbafda12e93d7d12aa7a0619140
J-D-O-S <juanolaya506@gmail.com>
2023-08-04 14:39:24 -0500

    Se añade una mejor distrubución con grid, además se añade una tamañano minimo y la prueba fue exitosa, los cambio pasan a la rama de dev-header

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 window.onload = (e) => {
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        centeredSlides: true,
-        loop: true,
-        spaceBetween: 30,
-        grabCursor: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        breakpoints: { // posible cambio para mayuscula
-            991: {
-                slidesPerView: 3
-            }
-        }
-    });
-}
+	var swiper = new Swiper(".mySwiper", {
+		slidesPerView: 1,
+		centeredSlides: true,
+		loop: true,
+		spaceBetween: 30,
+		grabCursor: true,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev"
+		},
+		breakpoints: {
+			// posible cambio para mayuscula
+			991: {
+				slidesPerView: 3
+			}
+		}
+	});
+
+	// necesito saber la altura de la pantalla
+	var altura = window.innerHeight;
+	console.log(altura);
+};
